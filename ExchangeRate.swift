87e5cf5f0b2fd3821a433c19a9d427feaffcbfd7
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-05-28 01:20:34 +0200

    Create ExchangeRate.swift

---
+++ ExchangeRate.swift
@@ -0,0 +1,11 @@
+// 🇨🇴 Pesos conversion rate: 0,045
+// 🇧🇷 Reais conversion rate: 0,19
+// 🇵🇪 Soles conversion rate: 0.29
+
+var pesos : Double = 1250
+var reais : Double = 1000
+var soles : Double = 1500
+var total : Double = 0
+
+total = pesos * 0,045 + reais * 0,19 + soles * 0,29
+print("US Dollars = $\(total)")
