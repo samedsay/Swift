51ecf9ad138e889ad9e6df89ac7a2b225d7c4937
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-07 22:37:58 +0200

    Create 014_SocksDrawer.swift

---
+++ 014_SocksDrawer.swift
@@ -0,0 +1,7 @@
+var sockDrawer: Set = ["Red Polka Dots", "Blue Stripes", "Plain Whites"]
+
+if sockDrawer.isEmpty{
+  print("Time to add some more socks!")
+}else{
+  print("We have \(sockDrawer.count) pairs of socks.")
+}
