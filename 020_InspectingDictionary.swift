209dc0bd14164eba20e2f409014b1fdcc036427f
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-08 19:25:35 +0200

    Create 020_InspectingDictionary.swift

---
+++ 020_InspectingDictionary.swift
@@ -0,0 +1,13 @@
+var numberOfSides = [
+  "triangle": 3,
+  "square": 4,
+  "rectangle": 4,
+  "decagon": 10,
+  "triacontagon": 30
+]
+
+if numberOfSides.isEmpty{
+  print("This dictionary has no elements in it.")
+}else{
+  print("Number of elements : \(numberOfSides.count)")
+}
