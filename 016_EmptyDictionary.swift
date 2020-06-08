a219b1d3504accd4d7ceff45507fa5758d0671c1
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-08 18:24:51 +0200

    Update 016_EmptyDictionary.swift

--- 016_EmptyDictionary.swift
+++ 016_EmptyDictionary.swift
@@ -7,3 +7,7 @@ var roleModels : [String:String] = ["Mr. Rogers": "Fred McFeely Rogers",
                                     "The Crocodile Hunter": "Stephen Robert Irwin", 
                                     "Bill Nye the Science Guy": "William Sanford Nye"]
 print(roleModels)
+
+// With Type Inference; We don't need to use ':[String:Int]' part.
+var movieYears = ["Finding Nemo":2003, "Toy Story":1995]
+print(movieYears)
