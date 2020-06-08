1ac1976f06e7ba8a36d40245ed380c723fb59aa0
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-08 18:21:05 +0200

    Update 016_EmptyDictionary.swift

--- 016_EmptyDictionary.swift
+++ 016_EmptyDictionary.swift
@@ -2,3 +2,8 @@
 var emptyLiteral : [String:Int] = [:]
 // Initializer Syntax of Dictionary
 var emptyInitializer = [String:Bool]()
+
+var roleModels : [String:String] = ["Mr. Rogers": "Fred McFeely Rogers", 
+                                    "The Crocodile Hunter": "Stephen Robert Irwin", 
+                                    "Bill Nye the Science Guy": "William Sanford Nye"]
+print(roleModels)
