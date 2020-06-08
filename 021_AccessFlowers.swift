9ae3307c021b67f0c2739adb3d5c1a1a1c530dfd
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-08 19:36:37 +0200

    Update 021_AccessFlowers.swift

--- 021_AccessFlowers.swift
+++ 021_AccessFlowers.swift
@@ -15,3 +15,15 @@ print(sunflowerScientific)
 if let lilyScientific = flowerNames["Lily"]{
   print("A lily is referred to as a \(lilyScientific) in the science community.")
 }
+
+
+// For Loop for accessing key and value in dictionaries.
+var mythology = [
+  "Zeus": "Jupiter",
+  "Athena": "Minerva", 
+  "Poseidon": "Neptune",
+  "Demeter": "Ceres"]
+
+for (greekName, romanName) in mythology{
+  print("\(greekName) is also known as \(romanName)")
+}
