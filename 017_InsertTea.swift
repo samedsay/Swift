078d07a667b6f8397ce3a633be1807ba1957312e
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-08 18:28:11 +0200

    Create 017_InsertTea.swift

---
+++ 017_InsertTea.swift
@@ -0,0 +1,9 @@
+var teaSteepingTemperature = [
+  "Black": 212,
+  "Oolong": 185,
+  "White": 185
+]
+// Insert New Elements
+teaSteepingTemperature["Green"] = 185
+teaSteepingTemperature["Rooibos"] = 212
+print(teaSteepingTemperature)
