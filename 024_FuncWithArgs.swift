7297cfdc8db52a9ed4554da83e6b99affce961bb
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-09 22:58:49 +0200

    Update 024_FuncWithArgs.swift

--- 024_FuncWithArgs.swift
+++ 024_FuncWithArgs.swift
@@ -1,10 +1,15 @@
 
 func findCircumference(diameter : Double) -> Double{
-  
   return 3.14 * diameter
+}
+var result = findCircumference(diameter:10)
+print(result)
 
+/////////////////////////////////////////////////////
+
+func timeToDestination(distance:Int, speed:Int)->Int{
+  let time = distance / speed
+  return time
 }
 
-var result = findCircumference(diameter:10)
-
-print(result)
+print(timeToDestination(distance:6836, speed:560))
